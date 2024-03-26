9569fe836991f7483d8dd6e176a12b860e0135a1
Marian <italianomariano198@gmail.com>
2024-03-26 18:00:46 -0300

    fixing errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
     <div className="h-[100vh] w-full flex justify-center items-center">
-      <h1 className="text-5xl text-white">Liveblocks Firma Clone</h1>
+      <h1 className="text-5xl text-white">Liveblocks Figma Clone</h1>
+
+      <Live />
     </div>
   );
 }
